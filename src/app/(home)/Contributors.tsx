0d3da72bf5d22c1d6863757d8b8eac7796106c06
Abdón Rodríguez <abdon@ibm.com>
2025-12-03 12:41:35 -0500

    Add University of Maryland as contributor

--- src/app/(home)/Contributors.tsx
+++ src/app/(home)/Contributors.tsx
@@ -3,12 +3,14 @@ import AlgorithmiqLogo from './logo_algorithmiq.png';
 import BluequbitLogo from './logo_bluequbit.png';
 import IbmLogo from './logo_ibm.png';
 import RikenLogo from './logo_riken.png';
+import UMarylandLogo from './logo_uMaryland.png';
 
 const contributors = [
   { name: 'Algorithmiq', logo: AlgorithmiqLogo },
   { name: 'BlueQubit', logo: BluequbitLogo },
   { name: 'IBM', logo: IbmLogo },
   { name: 'RIKEN', logo: RikenLogo },
+  { name: 'University of Maryland', logo: UMarylandLogo },
 ];
 
 export function Contributors() {
